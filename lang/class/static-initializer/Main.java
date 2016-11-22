1fe3ce58a403d122df073a68c1382abfb7fb02b0
Ryo Akagi <ryoakg@gmail.com>
2016-11-23 07:42:35 +0900

    static-initializer

---
+++ lang/class/static-initializer/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    static {
+        System.out.println("in static initializer.");
+    }
+    static public void main(String... args){
+        System.out.println("in main().");
+    }
+}
